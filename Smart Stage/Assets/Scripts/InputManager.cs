4907cb50077c8b2dd477f956e0735fe62135fc4b
Barkın ZORLU <83545665+ZorluBarkin@users.noreply.github.com>
2022-05-26 21:40:11 +0300

    Commit 4
    Changed input system from mouse button to touch for use in Mobile applications with touch sensors

--- Smart Stage/Assets/Scripts/InputManager.cs
+++ Smart Stage/Assets/Scripts/InputManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
@@ -11,13 +13,21 @@ public class InputManager : MonoBehaviour
 
     private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
 
+    private Touch touch;
+
     // Update is called once per frame
     void Update()
     {
 
+        touch = Input.GetTouch(0);
+        if (Input.touchCount < 0 || touch.phase != TouchPhase.Began)
+            return;
+
+        if (IsPointerOverUI(touch)) return;
+
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = arCam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = arCam.ScreenPointToRay(touch.position);
             if(_raycastManger.Raycast(ray, _hits))
             {
                 Pose pose = _hits[0].pose;
@@ -26,4 +36,14 @@ public class InputManager : MonoBehaviour
         }
 
     }
+
+    private bool IsPointerOverUI(Touch touch)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = new Vector2(touch.position.x, touch.position.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results.Count > 0;
+    }
+
 }
