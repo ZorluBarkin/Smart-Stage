1922bc1ce8ddbad4982922d166436a20101506ac
Barkın ZORLU <83545665+ZorluBarkin@users.noreply.github.com>
2022-05-26 20:00:07 +0300

    Commit 1
    Part1 done

---
+++ Smart Stage/Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private GameObject arObj;
+    [SerializeField] private Camera arCam;
+    [SerializeField] private ARRaycastManager _raycastManger;
+
+    private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = arCam.ScreenPointToRay(Input.mousePosition);
+            if(_raycastManger.Raycast(ray, _hits))
+            {
+                Pose pose = _hits[0].pose;
+                Instantiate(arObj, pose.position, pose.rotation);
+            }
+        }
+
+    }
+}
